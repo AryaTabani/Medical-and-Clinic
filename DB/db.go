c1abb0fd8279e919d0e99df92e9d278996498a13
AryaTabani <a.tabani82@gmail.com>
2025-07-23 18:37:50 +0330

    updating database for testinomials and ask doctors

--- DB/db.go
+++ DB/db.go
@@ -36,17 +36,18 @@ func createtables() {
 		panic("Failed to create clinics table")
 	}
 
-	
 	createDoctorsTable := `
 	CREATE TABLE IF NOT EXISTS doctors (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		name TEXT NOT NULL,
 		specialty TEXT NOT NULL,
+		image_url TEXT,
+		approval_rating INTEGER,
+		years_of_experience INTEGER,
 		clinic_id INTEGER,
 		is_active BOOLEAN NOT NULL DEFAULT true,
 		FOREIGN KEY (clinic_id) REFERENCES clinics(id)
 	);`
-
 	_, err = DB.Exec(createDoctorsTable)
 	if err != nil {
 		panic("Failed to create doctors table")
@@ -62,5 +63,18 @@ func createtables() {
 	_, err = DB.Exec(createFacilitiesTable)
 	if err != nil {
 		panic("Failed to create doctors table")
+	}
+	createTestimonialsTable := `
+	CREATE TABLE IF NOT EXISTS testimonials (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		quote TEXT NOT NULL,
+		rating INTEGER NOT NULL,
+		author_name TEXT NOT NULL,
+		author_role TEXT,
+		author_image_url TEXT
+	);`
+	_, err = DB.Exec(createTestimonialsTable)
+	if err != nil {
+		panic("Failed to create testimonials table")
 	}
 }
